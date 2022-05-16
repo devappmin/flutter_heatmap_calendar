d47f2639b55318687a1e1fae755a286edca0e975
Iqbal Barikzai <iqbal.barikzai@gmail.com>
2022-05-16 09:49:13 -0700

    fixed bug where app would have overflow crash if start date was today

--- lib/src/widget/heatmap_month_text.dart
+++ lib/src/widget/heatmap_month_text.dart
@@ -46,7 +46,7 @@ class HeatMapMonthText extends StatelessWidget {
         // Add Text without width margin if first week is end of the month.
         // Otherwise, add Text with width margin.
         items.add(
-          label == 0 && firstDayInfos![label] != firstDayInfos![label + 1]
+          firstDayInfos!.length == 1 || (label == 0 && firstDayInfos![label] != firstDayInfos![label + 1])
               ? _renderText(DateUtil.SHORT_MONTH_LABEL[firstDayInfos![label]])
               : Container(
                   width: (((size ?? 20) + (margin?.right ?? 2)) * 2),
